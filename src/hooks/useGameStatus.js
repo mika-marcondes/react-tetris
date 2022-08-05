4b8b7a6bb5377c936a54229b5e136a12a4804067
Alef <felipexk1@live.com>
2022-08-05 20:54:57 +0000

    Memoization is important kiddo

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,18 +1,18 @@
-import {useEffect, useState, useCallback} from "react";
+import {useEffect, useState, useCallback, useMemo} from "react";
 
 export const useGameStatus = rowsCleared => {
     const [score, setScore] = useState(0)
     const [rows, setRows] = useState(0)
     const [level, setLevel] = useState(0)
 
-    const linePoints = [40, 100, 300, 1200]
+    const linePoints = useMemo(() => [40, 100, 300, 1200], []);
 
     const calcScore = useCallback(() => {
         if (rowsCleared > 0) {
             setScore(prevState => prevState + linePoints[rowsCleared - 1] * (level + 1))
             setRows(prevState => prevState + rowsCleared)
         }
-    }, [level, rowsCleared])
+    }, [level, linePoints, rowsCleared])
 
     useEffect(() => {
         calcScore()
